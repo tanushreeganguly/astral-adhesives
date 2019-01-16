daebc91c8d2bb8799b721fc3888934aeb3f60f2a
tanushreeganguly <39189417+tanushreeganguly@users.noreply.github.com>
2019-01-16 12:08:56 +0530

    admin files 16-1-19

---
+++ export_contacts.php
@@ -0,0 +1,77 @@
+<?php
+require_once("../config/config.php");
+require_once("verify_logins.php");
+
+date_default_timezone_set('Asia/Calcutta'); 
+
+
+$select     = $objTypes->select("tbl_contact", "*", "", "", "id DESC");
+
+if($select && count($select) > 0){
+	
+
+	$data 	= "<table border='1'>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Mobile Number</th>
+						<th>Email</th>
+						<th>Type</th>
+						<th>message</th>
+						<th>Date</th>
+						
+					</tr>
+				</thead>
+				";
+	$i=1;
+	foreach($select  as $rows)
+	{
+			$where[':spare_service_id'] =  $rows['id'];
+			$where_state[':state_id']   =  $rows['state'];
+			
+			//$select_images     = $objTypes->select("tbl_enquiry_images", "*", "spare_service_id=:spare_service_id", $where, "id DESC");
+			
+			//$select_state    = $objTypes->select("tbl_state_master", "*", "state_id=:state_id", $where_state, "state_id DESC");
+			
+			$data .= "<tr>
+						
+						<td>".$rows['name']."</td>
+						
+						<td>".$rows['mobile']."</td>
+						
+						<td>".$rows['email']."</td>
+						
+						<td>".$rows['contacttype']."</td>
+						<td>".$rows['message']."</td>
+						<td>".$rows['insert_date']."</td>";
+						
+					$data .= "</tr>";
+		
+		
+	}
+	
+	$data 		.='</tbody></table>';	
+	$html 		 = trim($data);
+	$currentdate = date("Y-m-d H:i:s");
+
+	//print $data;exit;
+	header("Expires: 0");
+	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+	header("Cache-Control: no-store, no-cache, must-revalidate");
+	header("Content-Type: application/vnd.ms-excel; charset=utf-8");
+	header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+	header("Content-Disposition: attachment;filename=\"$filename.xlsx\"");
+	header("Cache-Control: max-age=0");
+
+	header("Content-Disposition: attachment; filename=Contact_Report_".$currentdate.".xls");
+	header("Cache-Control: public");
+	header("Content-length: ".strlen($data));
+	header("Pragma: no-cache");
+	header("Expires: 0");
+	print $data;
+}else{
+	echo 'No Records!';
+}
+?>
--- fetch_brand.php
+++ fetch_brand.php
@@ -1,35 +1,21 @@
-<?php 
-include_once('config/config.php');
-$POST   = $objTypes->validateUserInput($_REQUEST);
+<?php
+require_once("../config/config.php");
+require_once("verify_logins.php");
 
 
-if(isset($POST["limit"], $POST["start"]))
-{
+$category_id=$_REQUEST['category_id'];
+if($category_id!=''){
+$TypeArray	= $objTypes->fetchAll("SELECT * FROM tbl_brands WHERE parent_category_id='".$category_id."' and is_active=1 and is_delete=1");
+$data='<select class="form-control" name="brand_id" id="brand_id2" style="width: 40%">
+                    <option value="">Select Brand</option>';
 
-
-$sqlbrandlist=$objTypes->fetchAll("select a.title,a.thumbnail1,a.id,a.description from tbl_products_details as b left join  tbl_brands as a on b.brand_id=a.id where a.is_active=1 and a.is_delete=1 group by a.id order by a.sortorder asc limit ".$POST["start"].",".$POST["limit"]);
-$data_show='';
-   if(count($sqlbrandlist)>0)
-   {
-              foreach($sqlbrandlist as $brandlist)
-              {
-              	$data_show.='<div class="product_blurb">
-              <a href="'.base_url.'b/'.$objTypes->prepare_url(stripslashes($brandlist['title'])).'-'.$brandlist['id'].'">
-              <h3><span>'.stripslashes($brandlist['title']).'</span></h3>
-              <div class="product_list">
-                <div class="img_hld">
-                    <img src="'.base_url.'uploads/brand_images/'.$brandlist['thumbnail1'].'" alt="">
-                </div>
-               
-              </div>
-              <div class="explore_product">
-                <img src="'.base_url.'assets/images/icon-plus.png" alt="">
-              </div>
-            </a>
-            </div>';
-              }
-          
-
-    }
-echo $data_show;
- }
+foreach($TypeArray as $parent)
+				{
+					
+					//echo $parent['title'];
+					$data .="<option value='".$parent['id']."'>".stripslashes($parent['title'])."</option>";		
+				}
+$data .='</select>';
+				echo $data;
+}
+?>
